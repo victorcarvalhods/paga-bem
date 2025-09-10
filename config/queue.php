e5c93c5547651f8ada0a60596e01f68cf9d305f9
Victor Carvalho <victor1carvalho2002.vc@gmail.com>
2025-09-10 12:04:50 -0300

    feat(queue, observalibilite): add horizon to controll queues

--- config/queue.php
+++ config/queue.php
@@ -69,7 +69,7 @@ return [
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
             'block_for' => null,
-            'after_commit' => false,
+            'after_commit' => true,
         ],
 
     ],
